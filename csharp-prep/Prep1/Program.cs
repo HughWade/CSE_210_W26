366dd7d45553926e703e2397ff403ab40260961d
Hugh Wade <wade.hugh@gmail.com>
2026-02-24 10:05:47 -0700

    Wrote code for prep1. Code is working.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        //Console.WriteLine("Hello Prep1 World!");
+        string firstName;
+        string lastName;
+
+        Console.Write("What is your first name: ");
+        firstName = Console.ReadLine();
+
+        Console.Write("What is your last name: ");
+        lastName = Console.ReadLine();
+
+        Console.WriteLine($"My name is {lastName}, {firstName} {lastName}");
     }
 }
