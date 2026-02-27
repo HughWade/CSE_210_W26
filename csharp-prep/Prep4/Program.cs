b76f944e380f883eb0ca135bd5666e417efd984d
Hugh Wade <wade.hugh@gmail.com>
2026-02-26 17:55:35 -0700

    Did some sandbox work and completed Prep #3 and #4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int toAdd = -1;
+
+        List<int> allNumbers = new List<int>();
+
+        while (toAdd!=0)
+        {
+            Console.Write("Enter a number: ");
+            toAdd = int.Parse(Console.ReadLine());
+            if (toAdd!=0)
+            {
+            allNumbers.Add(toAdd);
+            }
+
+            else
+            break;
+        }
+
+        float length = allNumbers.Count;
+
+        int sum = 0;
+        for (int i=0; i<length; i++)
+        {
+            sum+=allNumbers[i];
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average; 
+        average = sum / length;
+        Console.WriteLine($"The average is: {average}");
+
+        int largestNumber = 0;
+        for (int i=0; i<allNumbers.Count; i++)
+        {
+            if (allNumbers[i] > largestNumber)
+            {
+                largestNumber = allNumbers[i];
+            }
+        }
+        Console.WriteLine($"The largest number is: {largestNumber}");
+
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,67 @@
 using System;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
+
+
+    public static void firstFunction()
+        {
+        Console.WriteLine("Bob");
+        }
+
+    public static int addNumbers(int n1, int n2)
+    {
+        return n1+n2;
+    }
+
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+
+        // firstFunction();
+
+        // Console.WriteLine(addNumbers(1,2));
+        // for (int i=100000; i>=-100000; i-=10000)
+        // {
+        //     Console.WriteLine(i);
+        // }
+
+        bool done = false;
+
+        while (!done)
+        {
+            Console.Write("Please input your age: ");
+            int age1 = int.Parse(Console.ReadLine());
+
+            if (age1<0)
+            {
+                Console.WriteLine("Please input valid age.");
+            }
+
+            if (age1>0)
+            {
+                Console.Write("Thanks!");
+                done = true;
+            }
+        }
+
+
+        // List<int> myData = new List<int>();
+
+        // myData.Add(11);
+        // myData.Add(12);
+
+        // foreach(int i in myData)
+        // {
+        //     Console.WriteLine(i);
+        // }
+
+        
+
+    
+
     }
+    
 }
