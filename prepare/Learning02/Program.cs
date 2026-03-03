412542cc474b810583c512ad50ff9159eb03b79c
Hugh Wade <wade.hugh@gmail.com>
2026-03-03 16:58:47 -0700

    finished working on Job and Resume Classes assignment

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,25 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Job myFirstJob = new Job();
-        myFirstJob._companyName = "Raising Cane's";
-        myFirstJob._jobTitle = "Crew Member";
-        myFirstJob._startYear = 2021;
-        myFirstJob._endYear = 2022;
-        myFirstJob.DisplayJob();
+        Job job1 = new Job();
+        job1._companyName = "Raising Cane's";
+        job1._jobTitle = "Crew Member";
+        job1._startYear = 2021;
+        job1._endYear = 2022;
+        // job1.DisplayJob();
 
         Job mySecondJob = new Job();
         mySecondJob._companyName = "DRC Foodworks";
         mySecondJob._jobTitle = "Dishwasher & Delivery Driver";
         mySecondJob._startYear = 2024;
         mySecondJob._endYear = 2026;
-        mySecondJob.DisplayJob();
+        // mySecondJob.DisplayJob();
 
         Resume myResume = new Resume();
         myResume._name = "Hugh Wade";
         myResume._jobs = new List<Job>();
-        myResume._jobs.Add(myFirstJob);
+        myResume._jobs.Add(job1);
         myResume._jobs.Add(mySecondJob);
         myResume.DisplayResume();
+
     }
 }
