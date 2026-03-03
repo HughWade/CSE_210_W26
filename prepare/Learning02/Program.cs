72e971b36634e4300465f010a925bb9b7393edbf
Hugh Wade <wade.hugh@gmail.com>
2026-03-03 09:38:28 -0700

    created Job class and tested it

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job myFirstJob = new Job();
+        myFirstJob._companyName = "Raising Cane's";
+        myFirstJob._jobTitle = "Crew Member";
+        myFirstJob._startYear = 2021;
+        myFirstJob._endYear = 2022;
+        myFirstJob.DisplayJob();
+
+        Job mySecondJob = new Job();
+        mySecondJob._companyName = "DRC Foodworks";
+        mySecondJob._jobTitle = "Dishwasher & Delivery Driver";
+        mySecondJob._startYear = 2024;
+        mySecondJob._endYear = 2026;
+        mySecondJob.DisplayJob();
     }
 }
