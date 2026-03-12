195bca50825f5e3023cea18229d0318096791457
Hugh Wade <wade.hugh@gmail.com>
2026-03-12 09:17:01 -0600

    attempted to write a program that was meant to practice try-catch blocks

--- sandbox/TryCatch/Program.cs
+++ sandbox/TryCatch/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+class Program
+{
+
+
+    public static int GetAge()
+        
+        {
+            bool done = false;
+            int age1 = -1;
+
+            while (!done)
+        {
+            try
+            {
+                Console.Write("Please Enter Your Age: ");
+                age1 = int.Parse(Console.ReadLine());
+
+                if (age1<0 || age1>125)
+                {
+                    done = true;
+                }
+
+                else
+                {
+                    Console.WriteLine("Please Enter a valid age (0-125) ");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Please enter a valid age. ");
+            }
+
+            }
+
+            return age1;
+        }
+        
+public static void Main(string[] args)
+    {
+        
+
+    }
+
+}
+        
