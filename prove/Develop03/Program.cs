0d1b5f770ae9642630a60ad7ae98613262d012be
Hugh Wade <wade.hugh@gmail.com>
2026-03-14 12:43:28 -0600

    Deleted unnecessary code in Program Main()

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -5,14 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Reference reference1 = new Reference();
-        Console.WriteLine(reference1.DisplayReference());
-
         Scripture scripture1 = new Scripture();
         scripture1.DisplayScripture(5, reference1);
 
         Reference reference2 = new Reference("Helaman", 6, 36);
-        Console.WriteLine(reference2.DisplayReference());
-
         Scripture scripture2 = new Scripture("And thus we see that the Lord began to pour out his Spirit upon the Lamanites, because of their easiness and willingness to believe in his words.");
         scripture2.DisplayScripture(3, reference2);
     }
