4e45281f771687fa5153f0fd9813023b071edd6e
Hugh Wade <wade.hugh@gmail.com>
2026-03-14 23:37:48 -0600

    Corrected scripture reference for scripture 3.

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -13,7 +13,7 @@ class Program
         scripture2.DisplayScripture(3, reference2);
 
         Reference reference3 = new Reference("1 Nephi", 2, 14, 15);
-        Scripture scripture3 = new Scripture("And it came to pass that my father did speak unto them in the valley of Lemuel, with power, being filled with the Spirit, until their frames did shake before him. And he did confound them, that they durst not utter against him; wherefore, they did as he commanded them. And my father dwelt in a tent.");
+        Scripture scripture3 = new Scripture("14 And it came to pass that my father did speak unto them in the valley of Lemuel, with power, being filled with the Spirit, until their frames did shake before him. And he did confound them, that they durst not utter against him; wherefore, they did as he commanded them. 15 And my father dwelt in a tent.");
         scripture3.DisplayScripture(5, reference3);
     
     }
