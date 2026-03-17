9d1bb7c91f245aa9fec19ace8b1108d85dc08ff5
Hugh Wade <wade.hugh@gmail.com>
2026-03-17 09:40:30 -0600

    Experimenting more with inheritance.

--- sandbox/PersonProject/Person.cs
+++ sandbox/PersonProject/Person.cs
@@ -23,7 +23,37 @@ class Person
         return $"Their name is {_firstname} {_lastname}. They are {_age} years old and weigh {_weight} pounds.";
     }
 
+    public int GetAge()
+    {
+        return _age;
+    }
 
+    public int GetWeight()
+    {
+        return _weight;
+    }
+
+    public void SetWeight(int weight)
+    {
+        if (weight < 50 || weight>500)
+        {
+            Console.WriteLine("Invalid Weight, your person is either a child or should seek medical help.");
+        }
+
+        else
+        _weight = weight;
+    }
+
+    public void SetAge(int age)
+    {
+        if (age<18 || age>100)
+        {
+            Console.WriteLine("Invalid Age, your person is either a child or too old to be working.");
+        }
+
+        else
+        _age = age;
+    }
 
 
 }
--- sandbox/PersonProject/Program.cs
+++ sandbox/PersonProject/Program.cs
@@ -4,9 +4,15 @@
     {
         Person myMan = new Person("Bob", "Ross", 55, 170);
         Console.WriteLine(myMan.GetPersonInformation());
+        Console.WriteLine();
 
         Doctor theGoat = new Doctor("Heart Monitor", "Hank", "Smith", 45, 200);
         Console.WriteLine(theGoat.GetDoctorInformation());
+        Console.WriteLine();
+
+        Police myPolice = new Police("Baseball Bat", "John", "Lasseter", 34, 199);
+        Console.WriteLine(myPolice.GetPoliceInformation());
+        Console.WriteLine();
     }
 
 }
