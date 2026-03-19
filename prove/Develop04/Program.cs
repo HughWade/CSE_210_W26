f3c9fdffd628bd62e02666640fd3d0c63a40610a
Hugh Wade <wade.hugh@gmail.com>
2026-03-19 10:02:09 -0600

    Started working on the Main() function in the Program class.

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        Menu menu = new Menu();
+        
+        int number = menu.ProcessMenu();
+
+        switch(number)
+        {
+            case 1:
+            Activity activity1 = new Activity("Breathing", "");
+            break;
+
+            case 2:
+            Activity activity2 = new Activity("Reflecting", "");
+            break;
+
+            case 3:
+            Activity activity3 = new Activity("Listing", "");
+            break;
+
+
+
+
+
+        }
     }
 }
